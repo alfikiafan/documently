e5f6888ecd5b7a4e687ac6be64820db318bbd596
Alfiki Diastama Afan Firdaus <alfiki.diastama@gmail.com>
2023-01-28 22:29:43 +0700

    Update Responsivity

--- js/script.js
+++ js/script.js
@@ -29,53 +29,6 @@ $(document).ready(function(){
         });
     });
 
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav',
-        adaptiveHeight: true,
-        useTransform: true,
-        speed: 400,
-        cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
-        autoplay: true,
-        autoplaySpeed: 3500,
-    });
-
-    $('.slider-nav').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true,
-        adaptiveHeight: true,
-        arrows: true,
-        autoplay: true,
-        autoplaySpeed: 3500,
-
-        responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            }
-        }, {
-            breakpoint: 640,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 3,
-            }
-        }, {
-            breakpoint: 420,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 3,
-            }
-        }]
-    });
-
     $("a").on('click', function(event) {
 
         if (this.hash !== "") {
