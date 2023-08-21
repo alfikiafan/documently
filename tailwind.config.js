af6cf1eec0981e6095228f5e3d637977781ec3b2
Rio Saputro <rio.ae23@gmail.com>
2023-08-21 21:07:28 +0700

    Add Tailwind and NPM

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['coba.html'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'ijo':'#0df5e3'
+        
+      },
+      colors: {
+        'ijo':'#0df5e3'
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
